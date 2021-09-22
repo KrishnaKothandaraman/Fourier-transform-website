1ad9e3b246a41751e14e6b7d2dcd96e586e486bf
krishna712001 <63452545+krishna712001@users.noreply.github.com>
2021-09-22 23:23:15 +0800

    Integrated canvas

--- fourier_transform_website/urls.py
+++ fourier_transform_website/urls.py
@@ -21,5 +21,7 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', home)
+    path('', home),
+    path('userDrawing/', userDrawing),
+    path('imageDrawing/', imageDrawing)
 ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
--- fourier_transform_website/views/views.py
+++ fourier_transform_website/views/views.py
@@ -3,3 +3,9 @@ from django.shortcuts import render
 
 def home(request):
     return render(request, "home.html", {})
+
+def userDrawing(request):
+    return render(request, "userDrawing.html", {})
+
+def imageDrawing(request):
+    return render(request, "imageDrawing.html", {})
